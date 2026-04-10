963c364601374f426735468dcbe89564dbab172a
Avishka <avishkaindumini@gmail.com>
2026-04-10 20:43:00 +0530

    refactor:  CreateCheckoutSessionDto

--- backend/payment-service/src/payments/dto/create-intent.dto.ts
+++ backend/payment-service/src/payments/dto/create-intent.dto.ts
@@ -1,11 +1,13 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateIntentDto {
+  // The appointment ID for which the payment intent is being created.
   @IsString()
   @MinLength(20)
   @MaxLength(32)
   appointmentId!: string;
 
+  // The email address of the patient for whom the payment intent is being created.
   @IsEmail()
   patientEmail!: string;
 }
