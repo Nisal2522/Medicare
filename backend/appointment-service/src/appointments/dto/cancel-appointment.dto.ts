8e581200898c30be5292182c10b10f95eac8b51a
Avishka <avishkaindumini@gmail.com>
2026-04-11 20:43:00 +0530

    refactor: Create  BookAppointmentDto

--- backend/appointment-service/src/appointments/dto/cancel-appointment.dto.ts
+++ backend/appointment-service/src/appointments/dto/cancel-appointment.dto.ts
@@ -1,5 +1,6 @@
 import { IsEmail } from 'class-validator';
 
+// DTO for canceling an appointment. It contains the email address of the patient who wants to cancel the appointment.
 export class CancelAppointmentDto {
   @IsEmail()
   patientEmail!: string;
